f9a6d1b40188531be8b9e9ca0affa9e5fa228fab
jimni6 <nijimmy@outlook.fr>
2024-10-01 13:42:31 +0200

    chore: removed unused files and code

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,31 @@ import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
 
 import { AuthProvider } from './context/AuthProvider';
 
-
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from './firebase'; // Import the Firestore instance
-
-// useEffect(() => {
-  //   const fetchTest = async () => {
-    //     const querySnapshot = await getDocs(collection(db, 'test'));
-    //     querySnapshot.forEach((doc) => {
-      //       console.log(`${doc.id} => ${doc.data()}`);
-      //     });
-      //   };
-      
-      //   fetchTest();
-      // }, []);
-      const App: React.FC = () => {
-        return (
-          <AuthProvider>
-            <Router>
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                {/* Protect the home route with PrivateRoute */}
-                <Route
-                  path="/home"
-                  element={
-                    <PrivateRoute>
-                      <Home />
-                    </PrivateRoute>
-                  }
-                />
-                <Route
-                  path="/dashboard"
-                  element={
-                    <PrivateRoute requiredRole="admin">
-                      <Dashboard />
-                    </PrivateRoute>
-                  }
-                />
-              </Routes>
-            </Router>
-          </AuthProvider>
-        );
-      };
+  const App: React.FC = () => {
+    return (
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            {/* Protect the home route with PrivateRoute */}
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute requiredRole="admin">
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    );
+  };
 
 export default App;
